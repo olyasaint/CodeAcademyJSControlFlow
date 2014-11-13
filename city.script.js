8e0491e8375e6788e90d7fd3f342abfa55218f4f
olyasaint <olya@ostryk.com>
2014-11-12 20:40:03 -0800

    Create city.script.js

---
+++ city.script.js
@@ -0,0 +1,14 @@
+var city = prompt ("What city would you like to live in?", "Type your city here")
+switch (city) {
+    case 'San Francisco':
+        console.log("It is gorgeous there!");
+        break;
+    case 'Paris':
+        console.log("Food, love and gorgeous women: you must have good taste");
+        break;
+    case 'Dubai':
+        console.log("One word: luxuary");
+        break;
+    default:
+    console.log("I hope you catch a travel bug soon");
+}
